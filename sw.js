10a0d07fab4b8a851add9c5a453ba7cd7e23efc2
medaka-kazu <kazu10128501@gmail.com>
2026-07-13 06:43:58 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='hasunuma-pg-v1',F=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
